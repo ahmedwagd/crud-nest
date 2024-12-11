e58fa89f62e001933e81df1c34731aa669b33020
Ahmed Wagdy <ahmedwa122@gmail.com>
2024-12-11 11:32:13 +0200

    MOD: another way to implement PrismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {
-  public prismaClient: PrismaClient;
-  constructor() {
-    this.prismaClient = new PrismaClient();
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    // Note: this is optional
+    await this.$connect();
   }
 }
+
+// import { Injectable } from '@nestjs/common';
+// import { PrismaClient } from '@prisma/client';
+
+// @Injectable()
+// export class PrismaService {
+//   public prismaClient: PrismaClient;
+//   constructor() {
+//     this.prismaClient = new PrismaClient();
+//   }
+// }
